08d06c3deeee6823197ab98a62d520b14abb6b66
Advait Nair <advaitincorporated@gmail.com>
2025-09-21 21:57:54 +0100

    triggering an update?

--- utils/updater.py
+++ utils/updater.py
@@ -8,6 +8,7 @@ update_time = properties_cfg.get("update_schedule_time")
 def update():
     subprocess.run("git pull".split(' '))
     subprocess.run("python3.12 .".split(' '))
+    
     # subprocess.run("git pull \n && python3.12 .".split(' '))
 
 def check_update():
