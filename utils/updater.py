34158d3003ee0179816d3460eeb96aadfba77692
Advait Nair <advaitincorporated@gmail.com>
2025-09-21 21:38:04 +0100

    Refactor update and check_update functions for improved git commands

--- utils/updater.py
+++ utils/updater.py
@@ -6,10 +6,10 @@ import subprocess
 update_time = properties_cfg.get("update_schedule_time")
 
 def update():
-    subprocess.run("git pull https://github.com/Advait-Nair/AdvaCCTV.git ; python3.12 .".split(' '))
+    subprocess.run("git pull ; python3.12 .".split(' '))
 
 def check_update():
-    needs_update = not "up to date" in subprocess.run("git status | grep \"up to date\"".split(' '),encoding="utf-8", stdout=subprocess.PIPE).stdout
+    needs_update = not "up to date" in subprocess.run("git remote update main ; git status | grep \"up to date\"".split(' '),encoding="utf-8", stdout=subprocess.PIPE).stdout
     
     if needs_update:
         print("\n\nPreparing update...")
