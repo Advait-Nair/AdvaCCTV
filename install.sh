d9eee5bb3c0b6cb53e107eb4740912663969bb77
Advait Nair <advaitincorporated@gmail.com>
2025-09-22 20:58:13 +0100

    Refactor Python installation logic in install script; improve package manager detection and streamline dependency installation

--- install.sh
+++ install.sh
@@ -10,8 +10,23 @@ if ! command -v python3.12 &> /dev/null
 then
     # install 3.12
     echo "Python 3.12 not found. Installing..."
-    sudo apt update
-    sudo apt install -y python3.12 python3.12-venv
+    
+    # Detect the package manager
+    if command -v apt &> /dev/null; then
+        sudo apt update
+        sudo apt install -y python3.12 python3.12-venv
+    elif command -v dnf &> /dev/null; then
+        sudo dnf install -y python3.12 python3.12-devel
+    elif command -v yum &> /dev/null; then
+        sudo yum install -y python3.12 python3.12-devel
+    elif command -v pacman &> /dev/null; then
+        sudo pacman -Sy python python-pip
+    elif command -v brew &> /dev/null; then
+        brew install python@3.12
+    else
+        echo "Could not detect package manager. Please install Python 3.12 manually."
+        exit 1
+    fi
 else
     echo "Python 3.12 found."
 fi
@@ -19,7 +34,8 @@ fi
 
 # Ask use if they want to create a venv
 read -p "Do you want to create a virtual environment? If not, pip flag --break-system-packages will install everything needed globally, and possibly break another Python project. (y/n): " create_venv
-if [[ "$create_venv" == "y" || "$create_venv" == "Y" ]]; then
+if [[ "$create_venv" == "y" || "$create_venv" == "Y" ]]
+then
     python3.12 -m venv ./.venv
     source ./.venv/bin/activate
     echo "Virtual environment created and activated."
@@ -28,7 +44,7 @@ if [[ "$create_venv" == "y" || "$create_venv" == "Y" ]]; then
 else
     echo "Skipping virtual environment creation."
     echo "Installing dependencies globally. This will use --break-system-packages. You might need to use 'sudo' for the next command"
-    sudo pip3.12 install -r requirements.txt --break-system-packages
+    pip3.12 install -r requirements.txt --break-system-packages
 fi
 
 echo "
