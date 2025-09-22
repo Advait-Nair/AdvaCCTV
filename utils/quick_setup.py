cef4b2711af5f67d0edd45d6d35230f5beccc1a2
Advait Nair <advaitincorporated@gmail.com>
2025-09-22 19:51:55 +0100

    added abort control to quick setup, bashrc shortcut implementation

--- utils/quick_setup.py
+++ utils/quick_setup.py
@@ -1,13 +1,25 @@
+from pathlib import Path
 import re
 from utils.systemctl_restarter import Restarter
 
 # Quick setup tool
 # Used to setup the config file on first run
 
-def QuickSetup():
+def _QuickSetup():
     print('\n'*11)
     print("AdvaCCTV Quick Setup\n")
 
+    # Append acctv to .bashrc if not already present
+    with open(f"{str(Path.home())}/.bashrc", "r") as f:
+        bashrc = f.read()
+        if "alias acctv=" not in bashrc:
+            with open(f"{str(Path.home())}/.bashrc", "a") as fa:
+                fa.write('\n# AdvaCCTV Alias\nalias acctv="cd ~/AdvaCCTV && python3.12 ."\n')
+                fa.close()
+
+    input("Bashrc alias has been created. Run shortcut is 'acctv'. Press Enter to continue with parameter modification, or ^C to abort...")
+    
+    
     server_ip= input("Enter the server IP to use : ")
     server_port= input("Enter the server port to use : ")
     server_mode= "true" if "t" in input("Is this a server? (true/false) : ") else "false"
@@ -30,7 +42,21 @@ def QuickSetup():
             write_f.close()
 
         f.close()
+
     
     autostart_req = True if 'y' in input ("Do you want to auto-setup systemd service? (Y/N) : ").lower() else False
     if autostart_req:
         Restarter()
+
+def QuickSetup():
+    try:
+        _QuickSetup()
+        print("\n\nSetup complete! Please restart the terminal or run 'source ~/.bashrc' to apply the alias.")
+        print("You can now run the program using the 'acctv' command from any directory.")
+    except KeyboardInterrupt:
+        print("\n\nSetup aborted!\n")
+        exit(1)
+    except Exception as e:
+        print(f"\n\nSetup failed: {e}")
+        print("Please fix the issue and run 'python3.12 . setup' again. Internal error? Log an issue on GitHub at Advait-Nair/AdvaCCTV.")
+        exit(1)
